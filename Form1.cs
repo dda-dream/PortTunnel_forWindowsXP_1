d3f4302ffe7f6983eeb68f3af6fa11c3941d0cb2
dima <dda.dream@gmail.com>
2024-04-25 09:51:57 +0300

    wrk

--- Form1.cs
+++ Form1.cs
@@ -4,13 +4,16 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
-
+ 
 namespace PortTunnel_forWindowsXP_1
 {
     public partial class Form1 : Form
     {
+        string[] settingArray;
         public Form1()
         {
             InitializeComponent();
@@ -18,9 +21,28 @@ namespace PortTunnel_forWindowsXP_1
 
         private void Form1_Shown( object sender, EventArgs e )
         {
-            string s = File.ReadAllText("Settings.txt");
-
+            string s = File.ReadAllText("PortTunnelSettings.txt");
             tb_Settings.Text = s;
         }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            settingArray = tb_Settings.Text.Split('\n');
+            listener_1();
+            //listener_2();
+
+        }
+
+        void listener_1()
+        {
+            TcpForwarderSlim tunnel = new TcpForwarderSlim();
+            tunnel.local = new IPEndPoint(IPAddress.Any, 4001);
+            tunnel.remote = new IPEndPoint(IPAddress.Parse("172.18.12.47"), 3389);
+
+            Thread InstanceCaller = new Thread(new ThreadStart(tunnel.Start));
+            InstanceCaller.Start();
+        }
+        
+        
     }
 }
